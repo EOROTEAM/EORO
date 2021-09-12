0507b0f2b3634db497d5060a9d564845b7b11d2e
EOROTEAM <90535342+EOROTEAM@users.noreply.github.com>
2021-09-12 03:48:17 +0300

    Add files via upload

--- RUNER.sh
+++ RUNER.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 THIS_DIR=$(cd $(dirname $0); pwd)
-Version=`lsb_release -rs | cut -f1 -d"."`
+VARsion=`lsb_release -rs | cut -f1 -d"."`
 cd $THIS_DIR
 install() {
 echo -e "\e[38;5;77m    •{ Welcome to install source EORO }•\e[0m"
@@ -15,9 +15,9 @@ sudo apt-get install tmux
 sudo apt-get install luarocks
 echo -e "\e[38;5;77m       •{ install source EORO [20%]  }•\e[0m"
 sudo apt-get install screen
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-sARvAR autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
 echo -e "\e[38;5;77m       •{ install source EORO [30%]  }•\e[0m"
-if [ "$Version" == "18" ]; then
+if [ "$VARsion" == "18" ]; then
 echo -e "\033[0;31m\n~ Installing Depedencies For Ubuntu 18... \n\033[0m"
 cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
 wget "ibotcorp.com/files/compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
@@ -34,24 +34,24 @@ echo -e "\e[38;5;77m       •{ install source EORO [50%]  }•\e[0m"
 sudo luarocks install luasocket
 sudo luarocks install luasec
 sudo luarocks install redis-lua
-sudo luarocks install lua-term
+sudo luarocks install lua-tARm
 echo -e "\e[38;5;77m       •{ install source EORO [60%]  }•\e[0m"
-sudo luarocks install serpent
+sudo luarocks install sARpent
 sudo luarocks install dkjson
 sudo luarocks install Lua-cURL
 sudo luarocks install luautf8
 echo -e "\e[38;5;77m       •{ install source EORO [70%]  }•\e[0m"
-sudo service redis-server start
+sudo sARvice redis-sARvAR start
 sudo apt-get install libconfig++9v5 -y 
 sudo apt-get install libstdc++6 -y
 echo -e "\e[38;5;77m       •{ install source EORO [80%]  }•\e[0m"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get install lua-lgi -y  
-sudo apt-get install software-properties-common -y
+sudo apt-get install software-propARties-common -y
 echo -e "\e[38;5;77m       •{ install source EORO [90%]  }•\e[0m"
 sudo apt-get install libnotify-dev -y 
 sudo apt-get install lua-space -y
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-sARvAR autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
 echo -e "\e[38;5;77m       •{ install source EORO [100%]  }•\e[0m"
 sudo apt-get update 
 sudo apt-get upgrade -y
@@ -64,12 +64,12 @@ echo -e "\e[38;5;77m       |  _/ _ \| |_| |_| | |_|  _ <\e[0m"
 echo -e "\e[38;5;77m       |_|/_/   \_\_|/|_|_| \_\\e[0m"
 
 echo -e "\e[38;5;77m       •{ EORO source has been installed }•\e[0m"
-echo -e "\e[38;5;77m       •{ Source developer : @KKKKF }•\e[0m"
+echo -e "\e[38;5;77m       •{ Source developAR : @KKKKF }•\e[0m"
 cd ..
 rm -rf luarocks*
 cd && cd $THIS_DIR
-chmod +x ./ER
-sudo sh ER
+chmod +x ./AR
+sudo sh AR
 }
 if [ "$1" = "install" ]; then
 install
@@ -79,6 +79,6 @@ echo "Tg Is Not Found"
 exit 1
 fi
 cd && cd $THIS_DIR
-chmod +x ./ER
-sudo sh ER
+chmod +x ./AR
+sudo sh AR
 fi
